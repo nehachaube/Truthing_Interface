369caef230020de1e8fdb2151464d387462ba683
Divya <Divya@192.168.1.108>
2015-08-03 10:58:12 -0500

    download as csv
    Change-Id: I2fb4ebc41ecb0b178d5f81df567d65ddb52571ae

--- Truthing_Interface/WebContent/src/js/ImageCaptioning.js
+++ Truthing_Interface/WebContent/src/js/ImageCaptioning.js
@@ -74,14 +74,21 @@ function viewModel() {
     				}
     				
     			}
-    			var csvString = csvRows.join("%0A");
-				var a = document.createElement('a');
-				a.href = 'data:attachment/csv,' + csvString;
-				a.target = '_blank';
-				a.download = 'myFile.csv';
-				
-				//document.body.appendChild(a);
-				a.click();
+    			
+    			var data = csvRows;
+    			var csvContent = "data:text/csv;charset=utf-8,";
+    			data.forEach(function(infoArray, index){
+
+    			   dataString = infoArray.join(",");
+    			   csvContent += index < data.length ? dataString+ "\n" : dataString;
+
+    			}); 
+    			var encodedUri = encodeURI(csvContent);
+    			var link = document.createElement("a");
+    			link.setAttribute("href", encodedUri);
+    			link.setAttribute("download", "my_data.csv");
+
+    			link.click();
 				document.getElementById("download").disabled = true;
     		},
     		error: function(response){
